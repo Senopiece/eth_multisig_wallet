18700a7903d485b7ae9beff3ca4e849014beca98
Nabuki <shichiromail@gmail.com>
2021-03-03 19:25:12 +0300

    add solc

--- src/deploy.py
+++ src/deploy.py
@@ -1,6 +1,8 @@
+import solc
 import sys
 import os
 from dotenv import load_dotenv
+from pprint import pprint
 
 load_dotenv(verbose=True, override=True)
 
@@ -8,13 +10,17 @@ RPCURL         = os.getenv('RPCURL')
 GASPRICE       = os.getenv('GASPRICE')
 WALLETCONTRACT = os.getenv('WALLETCONTRACT')
 SOLIDITY       = os.getenv('SOLIDITY')
-OWNERS         = os.getenv('OWNERS')
+OWNERS         = os.getenv('OWNERS').split()
 THRESHOLD      = os.getenv('THRESHOLD')
 VERIFY         = os.getenv('VERIFY')
+
+solc.install_solc(SOLIDITY)
 
 def main():
     if len(sys.argv) == 2:
         if os.path.exists(sys.argv[1]):
+            res = solc.compile_files([sys.argv[1]])
+            pprint(res)
             return
         else:
             problem = "There is no file: " + sys.argv[1]
