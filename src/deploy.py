8bc833a657a4a21c5d6aeb5d4af74010fe59e6c4
Nabuki <shichiromail@gmail.com>
2021-03-06 14:58:38 +0300

    AC-002-04 completed

--- src/deploy.py
+++ src/deploy.py
@@ -30,28 +30,31 @@ def main():
             intermediates = solcx.compile_files([filename]).get(filename+":"+WALLETCONTRACT)
 
             if intermediates is not None:
-                abi = intermediates["abi"]
-                code = intermediates["bin"]
-                
-                web3 = Web3(HTTPProvider(RPCURL)) # TODO: AC-002-04
-
-                tx = web3.eth.contract(bytecode=code, abi=abi).constructor(OWNERS, THRESHOLD).buildTransaction({
-                    'gasPrice': GASPRICE if GASPRICE is not None else web3.eth.gasPrice,
-                    'nonce': web3.eth.getTransactionCount(to_address(PRIVKEY))
-                })
-
-                signed = web3.eth.account.signTransaction(tx, private_key=PRIVKEY)
-
                 try:
-                    tx_hash = web3.eth.sendRawTransaction(signed.rawTransaction)
-                    txr = web3.eth.waitForTransactionReceipt(tx_hash)
-                    print("Deployed at " + txr['contractAddress'])
-                    return
-                except Exception as ex:
-                    if str(ex).find('Insufficient funds') != -1:
-                        problem = "The balance of the account " + to_address(PRIVKEY) + " is not enough to deploy."
-                    else:
-                        raise ex # Such conditions wasn't described
+                    web3 = Web3(HTTPProvider(RPCURL))
+
+                    abi = intermediates["abi"]
+                    code = intermediates["bin"]
+
+                    tx = web3.eth.contract(bytecode=code, abi=abi).constructor(OWNERS, THRESHOLD).buildTransaction({
+                        'gasPrice': GASPRICE if GASPRICE is not None else web3.eth.gasPrice,
+                        'nonce': web3.eth.getTransactionCount(to_address(PRIVKEY))
+                    })
+
+                    signed = web3.eth.account.signTransaction(tx, private_key=PRIVKEY)
+
+                    try:
+                        tx_hash = web3.eth.sendRawTransaction(signed.rawTransaction)
+                        txr = web3.eth.waitForTransactionReceipt(tx_hash)
+                        print("Deployed at " + txr['contractAddress'])
+                        return
+                    except Exception as ex:
+                        if str(ex).find('Insufficient funds') != -1:
+                            problem = "The balance of the account " + to_address(PRIVKEY) + " is not enough to deploy."
+                        else:
+                            raise ex # Such conditions wasn't described
+                except:
+                    problem = "The JSON RPC URL " + RPCURL + "is not accessible"
             else:
                 problem = "There is no contract `" + WALLETCONTRACT + "` in " + filename
         else:
